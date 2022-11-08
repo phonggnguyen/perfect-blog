3c3238dbff6541d77c631a7a098e88edac4f8a13
pinnnguyen <112460736+phonggnguyen@users.noreply.github.com>
2022-11-08 16:34:55 +0700

    remove vercel

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/bon/Documents/my-workspace/perfect-blog/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/bon/Documents/my-workspace/perfect-blog/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
