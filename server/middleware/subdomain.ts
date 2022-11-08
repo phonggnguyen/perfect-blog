dbc1d3e1e5fec48bd5f0940129d682ce5b8c1ea5
pinnnguyen <112460736+phonggnguyen@users.noreply.github.com>
2022-11-08 17:02:21 +0700

    init

--- server/middleware/subdomain.ts
+++ server/middleware/subdomain.ts
@@ -1,12 +1,12 @@
 export default defineEventHandler(({ req, res, context }) => {
-  const hostname = req.headers.host || ".meetruyen.com"
+  const hostname = req.headers.host || "meetruyen.com"
 
-  const mainDomain = ["localhost:3000", ".meetruyen.com"]
+  const mainDomain = ["localhost:3000", "meetruyen.com"]
 
   if (!mainDomain.includes(hostname)) {
     const currentHost =
       process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-        ? hostname.replace(`..meetruyen.com`, "")
+        ? hostname.replace(`.meetruyen.com`, "")
         : hostname.replace(`.localhost:3000`, "")
 
     console.log("currentHost", currentHost)
