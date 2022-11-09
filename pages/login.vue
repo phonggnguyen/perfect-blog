f7f04b90e7dac21d2d7b0e9885941577bd0096a9
pinnnguyen <112460736+phonggnguyen@users.noreply.github.com>
2022-11-09 11:38:21 +0700

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
 const client = useSupabaseClient()
 
 const signIn = async () => {
-  const { user } = await client.auth.signIn({ provider: "github" })
+  const { user } = await client.auth.signIn({ provider: "facebook" })
 
   console.log("user", user)
 }
@@ -18,6 +18,6 @@ const signIn = async () => {
   <div class="flex flex-col mx-auto max-w-48 mt-20">
     <h1 class="text-4xl font-bold text-center">Login</h1>
 
-    <button autofocus class="btn-primary mt-4" @click="signIn">Login with Github</button>
+    <button autofocus class="btn-primary mt-4" @click="signIn">Login with facebook</button>
   </div>
 </template>
