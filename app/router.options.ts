5ed2e31f592c8740f416e2475893a324084b70e2
pinnnguyen <112460736+phonggnguyen@users.noreply.github.com>
2022-11-09 08:32:52 +0700

    init

--- app/router.options.ts
+++ app/router.options.ts
@@ -7,7 +7,6 @@ export default <RouterOptions>{
     const subdomain = useSubdomain()
     if (ssrContext?.event.context.subdomain) subdomain.value = ssrContext?.event.context.subdomain
 
-    console.log("subdomain", subdomain)
     if (subdomain.value) {
       const userRoute = _routes.filter((i) => i.path.includes("/user/:siteId"))
       const userRouteMapped = userRoute.map((i) => ({
--- pages/user/[siteId].vue
+++ pages/user/[siteId].vue
@@ -6,6 +6,7 @@ const client = useSupabaseClient()
 const subdomain = useSubdomain()
 const profile = useSubdomainProfile()
 
+console.log("subdomain", subdomain.value)
 // this should fetch user's profiles and settings (if any)
 useAsyncData("profile", async () => {
   const { data, error } = await client
@@ -26,6 +27,6 @@ definePageMeta({
 <template>
   <div>
     <NuxtPage v-if="profile"></NuxtPage>
-    <div class="text-4xl my-20 font-bold text-center" v-else>Page not found</div>
+    <div class="text-4xl my-20 font-bold text-center" v-else>Page not found hehe</div>
   </div>
 </template>
